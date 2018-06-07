2177875bb98b09f40a2e44a7437453a9f590c172
irvingricardotenorio94 <irvingricardotenorio@gmail.com>
2018-06-06 20:41:09 -0700

    chiste corto modificado

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-n hombre llega a la farmacia en el preciso momento en que el farmacéutico iba a cerrarla ya que tiene prisa para llegar al tren.–¿Qué deseaba ? -le pregunta.–Sólo quería algo para sudar.–Aja… Tome estas dos maletas y acompáñeme con ellas hasta la estación.¡Deprisa!
+n hombre llega a la farmacia en el preciso momento en que el farmacéutico iba a
+cerrarla ya que tiene prisa para llegar al tren.–¿Qué deseaba ?
+    -le pregunta.–Sólo quería algo para sudar.–Aja… Tome estas dos maletas y acompáñeme con ellas hasta la estación.¡Deprisa!
